85b052437df6e38765601b1a07f9f6e14597b6f7
Thuy Nguyen <thuynguyenbk60@gmail.com>
2022-08-14 22:52:59 +0700

    change bucket

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-581997/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-581997/
+aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-103171825310/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-103171825310/
