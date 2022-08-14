39a8891d857c420ddca8fcb2a2604066c079f1c2
Thuy Nguyen <thuynguyenbk60@gmail.com>
2022-08-15 00:03:15 +0700

    update

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -9,5 +9,3 @@ export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
-export AWS_ACCESS_KEY_ID=ASIARQBMFD2PEM4HOLMO
-export AWS_SECRET_ACCESS_KEY=I6O8aMDatPliNL4B//IhdSTKzKtiPDsZTlpmhHnt
